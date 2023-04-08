065516c15f120ff482ff47d1bfce475c281cfc81
jfpazto <jfpazto@hotmail.com>
2023-04-07 21:10:39 -0600

    Creacion proyecto chat

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(4.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage(
+                "https://lumiere-a.akamaihd.net/v1/images/dua_lipa_portada_5_bf1628a4.jpeg?region=15,0,1956,1100&width=960"),
+          ),
+        ),
+        title: const Text("Mi Amor"),
+        centerTitle: true,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+                child: ListView.builder(
+              itemCount: 100,
+              itemBuilder: (context, index) {
+                return const MyMessageBubble();
+              },
+            )),
+            Text("Mundo")
+          ],
+        ),
+      ),
+    );
+  }
+}
