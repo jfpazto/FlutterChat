066318d021bac8914d7d5aa641619205a340e10f
jfpazto <jfpazto@hotmail.com>
2023-04-08 16:41:30 -0600

    Cambios complementados

---
+++ lib/presentation/widgets/shared/message_field_box.dart
@@ -0,0 +1,45 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class MessageFieldBox extends StatelessWidget {
+  const MessageFieldBox({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final textController = TextEditingController();
+    final focusNode = FocusNode();
+    final outlineInputBorder = UnderlineInputBorder(
+        borderSide: const BorderSide(color: Colors.transparent),
+        borderRadius: BorderRadius.circular(20));
+
+    final inputDecoration = InputDecoration(
+        hintText: 'End your message?',
+        enabledBorder: outlineInputBorder,
+        focusedBorder: outlineInputBorder,
+        filled: true,
+        suffixIcon: IconButton(
+          icon: const Icon(Icons.send_outlined),
+          onPressed: () {
+            final textValue = textController.value.text;
+            print('valor de la caja de texto, $textValue');
+            textController.clear();
+          },
+        ));
+
+    return TextFormField(
+      onTapOutside: (event) {
+        focusNode.unfocus();
+      },
+      controller: textController,
+      decoration: inputDecoration,
+      onFieldSubmitted: (value) {
+        print('Submit value $value');
+        textController.clear();
+        focusNode.requestFocus();
+      },
+    );
+  }
+}
